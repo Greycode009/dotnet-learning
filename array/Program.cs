985ec79591e9b04e6cd21243691cbfcba70f2055
Dipesh Malla <gamerdipesh002@gmail.com>
2026-07-23 09:28:45 +0545

    Method and Array learned

---
+++ array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+
+        Console.WriteLine("Array elements:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
