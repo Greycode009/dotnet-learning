3066e3d07d9995e215ec0737813ffb82442c1817
Dipesh Malla <gamerdipesh002@gmail.com>
2026-08-05 09:43:17 +0545

    Method Overriding

---
+++ Method_Overwriting/Program.cs
@@ -0,0 +1,24 @@
+﻿class Class1
+{
+    public virtual void display()
+    {
+        Console.WriteLine("Super class display method");
+
+    }
+}
+class Class2 : Class1
+{
+    public override void display()
+    {
+        Console.WriteLine("Sub class display method");
+    }
+}
+class Program
+{
+    static void Main(String[] args)
+    {
+        Class1 obj = new Class2();
+        obj.display();
+        Console.ReadKey();
+    }
+}
