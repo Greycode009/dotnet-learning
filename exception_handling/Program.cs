36bdcb6d197f052cbb83d36e4cb6d80747e4e25f
Dipesh Malla <gamerdipesh002@gmail.com>
2026-08-06 10:01:50 +0545

    Added User_defined_exception

--- exception_handling/Program.cs
+++ exception_handling/Program.cs
@@ -10,8 +10,9 @@ class MyClient
             div = 100/x;
             Console.WriteLine("Result is: {0}", div);   
         }
-        catch (DivideByZeroException e)// or we can use DivideByZeroException e to catch only divide by zero exception
+        catch (Exception e)// or we can use DivideByZeroException e to catch only divide by zero exception
         {
+            
             Console.WriteLine("An error occurred: {0}", e.Message); 
         }
         Console.WriteLine($"Result is {div}");
---
+++ user_defined_exception/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// using System;
+
+class MyException : Exception
+{
+    public MyException(string str)
+    {
+        Console.WriteLine("User defined exception" + str);
+    }
+}
+
+class MyClient
+{
+    public static void Main()
+    {
+        try
+        {
+            throw new MyException("This is the user defined exception");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception caught here: " + e.ToString());
+        }
+
+        Console.WriteLine("Program continues after the exception");
+    }
+}
