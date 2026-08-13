b23f18579cad48b1a781aa28557b46c3747fcfd4
Dipesh Malla <gamerdipesh002@gmail.com>
2026-08-13 09:53:27 +0545

    Stream_writer add

---
+++ Read_Write/Stream_Writer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace FileIOStreamWriterDemo
+{
+    class StreamWriterDemo
+    {
+        class WriteToFile
+        {
+            public void Data()
+            {
+                StreamWriter sw = new StreamWriter("demo.txt");
+                Console.WriteLine("Enter the text you want to write on File: ");
+                
+                //TO read the input from the user
+                string str = Console.ReadLine();
+                sw.WriteLine(str); //To write a line in buffer
+                sw.Flush();
+                sw.Close();
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteToFile wr = new WriteToFile();
+            wr.Data();
+            Console.ReadKey();
+        }
+    }
+}
