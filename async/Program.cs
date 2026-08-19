7464ae2bbe96da84bd787ae98570ab35a2cf4fa4
Dipesh Malla <gamerdipesh002@gmail.com>
2026-08-19 09:54:31 +0545

    Learned : LinkQue and Async function

---
+++ async/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task DownloadFileAsync(string fileName, int delayMs)
+    {   
+        Console.WriteLine($"Starting download: {fileName}");
+        await Task.Delay(delayMs);
+        Console.WriteLine($"Completed download: {fileName}");
+
+    }
+    static async Task Main()
+    {
+        Console.WriteLine("==== Download Started ====");
+
+        Task task1 = DownloadFileAsync("Document.pdf", 2000);
+        Task task2 = DownloadFileAsync("Image.jpg", 1000);
+
+        await Task.WhenAll(task1, task2);
+        Console.WriteLine("=== All Downloads finished! ===");
+
+    }
+}
---
+++ linkque/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>  {1,2,3,4,5,6,7,8,9,10};
+
+        var result = numbers.Where(n => n%2 == 0 ).Select(n => n * n).OrderByDescending(n => n);
+
+        Console.WriteLine("Processed Results:");
+        foreach(var val in result)
+        Console.WriteLine(val);
+    }
+}
